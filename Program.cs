c74548412697bcef7001308edb3a6071db3bee75
Say my name <91437948+Blurryfxce@users.noreply.github.com>
2022-09-21 11:43:08 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double r;
+            Console.WriteLine("Введiть радiус кулi: ");
+            r = double.Parse(Console.ReadLine());
+            double pi = 3.14;
+            double v = 4.0 / 3.0 * pi * (r * r * r);
+            double s = 4.0 * pi * (r * r);
+            Console.WriteLine("Об'єм = " + v);
+            Console.WriteLine("Площа поверхнi = " + s);
+
+
+        }
+    }
+}
