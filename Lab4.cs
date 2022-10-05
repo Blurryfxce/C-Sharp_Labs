c828ac4d5631257c82d645fbb75ca9b1dd0362bc
Say my name <91437948+Blurryfxce@users.noreply.github.com>
2022-10-05 12:48:11 +0300

    Add files via upload

---
+++ Lab4.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введiть розмiр масиву:\t");
+            int N = int.Parse(Console.ReadLine());
+            int[] arr = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write($"a[{i}]=");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            int max = arr[0];
+            int maxValue = 0;
+            for (int i = 0; i < N; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                    maxValue = i;
+                }
+            }
+            int min = arr[0];
+            int minValue = 0;
+            for (int i = 0; i < N; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                    minValue = i;
+                }
+            }
+            Console.WriteLine("Обробка масиву...");
+            if(minValue < maxValue)
+            {
+                for (int i = 0; i <= minValue; i++)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+                for (int i = maxValue - 1; i > minValue; i--)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+                for(int i = maxValue ; i < N; i++)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+            }
+            
+            else
+            {
+                for (int i = 0; i <= maxValue; i++)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+                for (int i = minValue - 1; i > maxValue; i--)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+                for (int i = minValue; i < N; i++)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+            }
+            
+
+        }
+    }
+}
+
