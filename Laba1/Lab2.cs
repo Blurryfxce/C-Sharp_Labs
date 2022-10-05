48699fd0019d0cf5d62e2353f9d56fbb46f46225
Say my name <91437948+Blurryfxce@users.noreply.github.com>
2022-10-05 12:02:50 +0300

    Add files via upload

---
+++ Laba1/Lab2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int nn;
+            int nk;
+            int k;
+            double res = 0;
+
+            nn = int.Parse(Console.ReadLine());
+            nk = int.Parse(Console.ReadLine());
+
+            for (k = nn; k <= nk; k++)
+            {
+                res += (Math.Pow(k, 2) - 3) / (Math.Pow(k, 2) - (Math.Pow(-1, k)) * k + 3);
+            }
+
+            Console.WriteLine(res);
+            Console.ReadLine();
+
+
+        }
+    }
+}
+
---
+++ Laba1/Lab3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, b, c;
+            bool isOpposite = false;
+            Console.WriteLine("Введiть 3 цiлi числа: ");
+            a = float.Parse(Console.ReadLine());
+            b = float.Parse(Console.ReadLine());
+            c = float.Parse(Console.ReadLine());
+
+            if (a % 1 != 0 || b % 1 != 0 || c % 1 != 0)
+            {
+                Console.WriteLine("Помилка! Числа не цілі");
+            }
+            else if (a + b == 0 || a + c == 0 || b + c == 0)
+            {
+                isOpposite = true;
+                Console.WriteLine(isOpposite + " (Взаємно протилежнi числа iснують)");
+            }
+            
+            else
+            {
+                Console.WriteLine(isOpposite + " (Взаємно протилежних чисел не iснує)");
+            }
+        }
+    }
+}
+
