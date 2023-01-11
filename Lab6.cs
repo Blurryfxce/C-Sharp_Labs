f7562ad82225845ce8789bd235256e35c90405da
Say my name <91437948+Blurryfxce@users.noreply.github.com>
2023-01-12 01:54:49 +0200

    Add files via upload

---
+++ Lab6.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Revers(string text)
+        {
+            string backwards = "";
+            for(int i = text.Length - 1; i >= 0; i--)
+            {
+                backwards += text[i];
+            }
+            Console.WriteLine(backwards);
+        }
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введiть текст або число: ");
+            string yourText = Console.ReadLine();
+            Revers(yourText);  
+        }
+    }
+}
